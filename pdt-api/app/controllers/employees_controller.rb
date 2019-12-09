5fbf668385f7a5388a166421501121601bd5f68f
cyantis <cyantis@gmail.com>
2019-12-09 16:11:35 -0700

    cleans up employees controller

--- pdt-api/app/controllers/employees_controller.rb
+++ pdt-api/app/controllers/employees_controller.rb
@@ -2,47 +2,47 @@ class EmployeesController < ApplicationController
   #before_action :require_login, :current_employee, except: [:new, :create, :index]
 
   def index
-    @employees = Employee.all
-    render json: @employees
+    employees = Employee.all
+    render json: employees
   end
 
-  def new
-    @employee = Employee.new
-  end
+  #def new
+  #  @employee = Employee.new
+  #end
 
   def create
-    @employee = Employee.create(employee_params)
-
-    if @employee.save
-      session[:user_id] =  @employee.id
-      flash[:message] = "Employee Created!"
-      redirect_to root_path
-    else
-      flash[:message] = "Required fields are missing. Please, enter again!"
-      render :new
-    end
+    employee = Employee.create(employee_params)
+    render json: employee
+    #if @employee.save
+    #  session[:user_id] =  @employee.id
+    #  flash[:message] = "Employee Created!"
+    #  redirect_to root_path
+    #else
+    #  flash[:message] = "Required fields are missing. Please, enter again!"
+    #  render :new
+    #end
   end
 
-  def show
-    @employee = Employee.find(params[:id])
-    render json: @employee
-  end
+  #def show
+  #  @employee = Employee.find(params[:id])
+  #  render json: @employee
+  #end
 
-  def edit
-    @employee = Employee.find(params[:id])
-  end
+  #def edit
+  #  @employee = Employee.find(params[:id])
+  #end
 
   def update
-    @employee = Employee.find(params[:id])
-    @employee.update(employee_params)
-
-    if @employee.save
-      flash[:message] = "Employee Updated!"
-      redirect_to employee_path(@employee)
-    else
-      flash[:message] = "Required fields are missing. Please, enter again!"
-      render :edit
-    end
+    employee = Employee.find(params[:id])
+    employee.update(employee_params)
+    render json: employee
+    #if @employee.save
+    #  flash[:message] = "Employee Updated!"
+    #  redirect_to employee_path(@employee)
+    #else
+    #  flash[:message] = "Required fields are missing. Please, enter again!"
+    #  render :edit
+    #end
   end
 
   private
