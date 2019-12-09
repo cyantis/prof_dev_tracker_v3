b1e55e299aeb295000d10cc84a875c16778f545f
cyantis <cyantis@gmail.com>
2019-12-09 16:08:26 -0700

    cleans up comments controller

--- pdt-api/app/controllers/comments_controller.rb
+++ pdt-api/app/controllers/comments_controller.rb
@@ -1,48 +1,49 @@
 class CommentsController < ApplicationController
   def index
-    @comments = Comment.all
-    render json: @comments
+    comments = Comment.all
+    render json: comments
   end
 
-  def new
-    @comment = Comment.new
-    @event = Event.find(params[:event_id])
-    @employee = Employee.find_by_id(session[:user_id])
-  end
+  #def new
+  #  @comment = Comment.new
+  #  @event = Event.find(params[:event_id])
+  #  @employee = Employee.find_by_id(session[:user_id])
+  #end
 
   def create
-    @comment = Comment.create(comment_params)
-    @event = Event.find(params[:event_id])
-    if @comment.save
-      redirect_to event_path(@event)
-    else
-      render :new
-    end
+    comment = Comment.create(comment_params)
+    event = Event.find(params[:event_id])
+    render json: comment
+    #if @comment.save
+    #  redirect_to event_path(@event)
+    #else
+    #  render :new
+    #end
   end
 
-  def show
-    @comment = Comment.find(params[:id])
-    render json: @comment
-  end
+  #def show
+  #  @comment = Comment.find(params[:id])
+  #  render json: @comment
+  #end
 
-  def edit
-    @comment = Comment.find(params[:id])
-    @event = Event.find(params[:event_id])
-    @employee = Employee.find_by_id(session[:user_id])
-  end
+  #def edit
+  #  @comment = Comment.find(params[:id])
+  #  @event = Event.find(params[:event_id])
+  #  @employee = Employee.find_by_id(session[:user_id])
+  #end
 
   def update
-    @comment = Comment.find(params[:id])
-    @event = Event.find(params[:event_id])
-    @comment.update(comment_params)
-    redirect_to event_path(@event)
+    comment = Comment.find(params[:id])
+    event = Event.find(params[:event_id])
+    comment.update(comment_params)
+    render json: comment
   end
 
   def destroy
-    @comment = Comment.find(params[:id])
-    @event = Event.find(params[:event_id])
-    @comment.destroy
-    redirect_to event_path(@event)
+    comment = Comment.find(params[:id])
+    event = Event.find(params[:event_id])
+    comment.destroy
+    head :no_content, status: :ok
   end
 
 
