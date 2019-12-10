7d66b2d8bfabb73f1c17fe31ca0e2ff0088696bf
cyantis <cyantis@gmail.com>
2019-12-10 14:11:44 -0700

    adds SignUp component & starts building out auth process

--- pdt-app/src/App.js
+++ pdt-app/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import './App.css';
+import Login from './components/accountComponents/Login'
+import SignUp from './components/accountComponents/SignUp'
 import Footer from './components/layoutComponents/Footer'
 import Header from './components/layoutComponents/Header'
 import ProfileMenu from './components/layoutComponents/ProfileMenu'
@@ -12,8 +16,11 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header />
-        <ProfileMenu />
         <Content />
+        <Router>
+          <Route path="/" component={Login} />
+          <Route path="/signup" component={SignUp} />
+        </Router>
         <Footer />
       </div>
     );
