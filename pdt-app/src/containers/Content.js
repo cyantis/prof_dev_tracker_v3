81b5767f9ed7c8171f66a731a932cdad814e9781
cyantis <cyantis@gmail.com>
2019-12-09 19:25:30 -0700

    removes events container & home component--they were extraneous

--- pdt-app/src/containers/Content.js
+++ pdt-app/src/containers/Content.js
@@ -1,23 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { connect } from 'react-redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import Home from '../components/layoutComponents/Home'
 import LearningLog from '../components/accountComponents/LearningProfile'
-import EventsContainer from './EventsContainer'
+import LearningEvent from '../components/learningEventComponents/LearningEvent'
+import NewEvent from '../components/learningEventComponents/NewEvent'
+import LearningEventList from '../components/learningEventComponents/LearningEventList'
 
 class Content extends React.Component {
   render() {
     return (
       <div className="Content">
         <Router>
-          <Route path="/home" component={Home} />
           <Route path="/learning" component={LearningLog} />
+          <Route path="/events/new" render={props => <NewEvent addLearningEvent={ this.props.addLearningEvent } />} />
         </Router>
-        <EventsContainer />
+        <LearningEventList events={this.props.events} />
       </div>
     );
   }
 };
 
-export default Content
+const mapStateToProps = ({ events }) => ({ events })
+
+const mapDispatchToProps = dispatch => ({
+  addLearningEvent: learning => dispatch({ type: 'ADD_EVENT', learning }),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Content)
