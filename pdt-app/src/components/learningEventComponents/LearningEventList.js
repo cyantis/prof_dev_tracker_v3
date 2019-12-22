93da6d78e9aeb72a615c367fbb680911b09780b9
cyantis <cyantis@gmail.com>
2019-12-22 09:27:58 -0700

    refactors LearningEventList component for better readability

--- pdt-app/src/components/learningEventComponents/LearningEventList.js
+++ pdt-app/src/components/learningEventComponents/LearningEventList.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
-const LearningEventList = props => {
-
-  return(
-    props.events.map(event => <li key={event.id}>{props.employees ? (props.employees.filter(employee => event.employees[0].id == employee.id)[0].name + ': ') : null}<a href={`/events/${event.id}`}>{event.name}</a> {event.date}</li>)
-  )
-}
+const LearningEventList = props =>
+    props.events.map(event =>
+      <li key={event.id}>
+        {
+          props.employees
+            ? (props.employees.filter(employee => event.employees[0].id == employee.id)[0].name + ': ')
+            : null
+        }
+        <a href={`/events/${event.id}`}>
+          {event.name}
+        </a>
+        {event.date}
+      </li>
+    )
 
 export default LearningEventList
