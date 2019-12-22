1eda3dfe4cac34757e3a7c31d8dd22a3d5c52eda
cyantis <cyantis@gmail.com>
2019-12-22 09:21:19 -0700

    cleans-up Header component, making it stateless & pure

--- pdt-app/src/components/layoutComponents/Header.js
+++ pdt-app/src/components/layoutComponents/Header.js
@@ -1,11 +1,8 @@
 import React from 'react'
 
-const Header = () => {
-  return (
-    <div className="Header">
-      <h1>Professional Development Tracker</h1>
-    </div>
-  )
-}
+const Header = () =>
+  <div className="Header">
+    <h1>Professional Development Tracker</h1>
+  </div>
 
 export default Header
