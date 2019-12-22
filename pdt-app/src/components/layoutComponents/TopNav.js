57872cb07db0fceca0e88a545ff40660c21531fc
cyantis <cyantis@gmail.com>
2019-12-22 09:23:41 -0700

    cleans-up TopNav component, making it stateless & pure

--- pdt-app/src/components/layoutComponents/TopNav.js
+++ pdt-app/src/components/layoutComponents/TopNav.js
@@ -1,17 +1,11 @@
 import React from 'react'
 import Button from './Button'
 
-class TopNav extends React.Component {
-
-  render() {
-    return (
+const TopNav = () =>
       <div className="TopNav">
         <Button url='/learning' text='Your Learning Log' />
         <Button url='/events/new' text='Log New Learning' />
         <Button url='/home' text='Home' />
       </div>
-    )
-  }
-}
 
 export default TopNav
